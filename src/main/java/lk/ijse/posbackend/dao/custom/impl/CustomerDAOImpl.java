068bb2be9c0f6a898089bad6d793d13e575f27a1
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-01 17:26:23 +0530

    extend customer dao

--- src/main/java/lk/ijse/posbackend/dao/custom/impl/CustomerDAOImpl.java
+++ src/main/java/lk/ijse/posbackend/dao/custom/impl/CustomerDAOImpl.java
@@ -1,4 +1,34 @@
 package lk.ijse.posbackend.dao.custom.impl;
 
-public class CustomerDAOImpl {
+import lk.ijse.posbackend.dao.custom.CustomerDAO;
+import lk.ijse.posbackend.entity.CustomerEntity;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class CustomerDAOImpl implements CustomerDAO {
+    @Override
+    public boolean save(CustomerEntity dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public List<CustomerEntity> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public CustomerEntity search(String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean update(CustomerEntity dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException {
+        return false;
+    }
 }
