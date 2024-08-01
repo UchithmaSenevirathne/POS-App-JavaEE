45c0652a31d13843467bf5973114539b758bd37d
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-01 20:08:12 +0530

    add webservlet annotation

--- src/main/java/lk/ijse/posbackend/controller/Customer.java
+++ src/main/java/lk/ijse/posbackend/controller/Customer.java
@@ -2,12 +2,13 @@ package lk.ijse.posbackend.controller;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+@WebServlet(urlPatterns = "/customer", loadOnStartup = 2)
 public class Customer extends HttpServlet {
     @Override
     public void init() throws ServletException {
