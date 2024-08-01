5c5e7e2b950fc7cf018989aa9f814fc6bf578355
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-01 16:18:01 +0530

    add do methods

--- src/main/java/lk/ijse/posbackend/controller/Customer.java
+++ src/main/java/lk/ijse/posbackend/controller/Customer.java
@@ -1,4 +1,36 @@
 package lk.ijse.posbackend.controller;
 
-public class Customer {
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class Customer extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
