e80a6718ddcdc0d2d6b0f34d5a747726564a0329
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-01 17:06:57 +0530

    create sql util

--- src/main/java/lk/ijse/posbackend/dao/SQLUtil.java
+++ src/main/java/lk/ijse/posbackend/dao/SQLUtil.java
@@ -1,4 +1,21 @@
 package lk.ijse.posbackend.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SQLUtil {
+    public static <T> T execute(Connection connection, String query, Object...args) throws SQLException {
+        PreparedStatement pstm = connection.prepareStatement(query);
+
+        for (int i = 0; i < args.length; i++) {
+            pstm.setObject((i+1), args[i]);
+        }
+
+        if(query.toUpperCase().startsWith("SELECT")){
+            return (T)pstm.executeQuery();
+        }
+        return (T)(Boolean)(pstm.executeUpdate() > 0);
+    }
+
 }
