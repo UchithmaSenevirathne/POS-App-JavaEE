8028a8ae9dec6cb69e1b74f3699d479f89aaba58
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-02 19:07:43 +0530

    create item dao

---
+++ src/main/java/lk/ijse/posbackend/dao/custom/ItemDAO.java
@@ -0,0 +1,8 @@
+package lk.ijse.posbackend.dao.custom;
+
+import lk.ijse.posbackend.dao.CrudDAO;
+import lk.ijse.posbackend.dao.SuperDAO;
+import lk.ijse.posbackend.entity.ItemEntity;
+
+public interface ItemDAO extends CrudDAO<ItemEntity> {
+}
---
+++ src/main/java/lk/ijse/posbackend/dao/custom/impl/ItemDAOImpl.java
@@ -0,0 +1,35 @@
+package lk.ijse.posbackend.dao.custom.impl;
+
+import lk.ijse.posbackend.dao.custom.ItemDAO;
+import lk.ijse.posbackend.entity.ItemEntity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ItemDAOImpl implements ItemDAO {
+    @Override
+    public boolean save(Connection connection, ItemEntity entity) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public List<ItemEntity> getAll(Connection connection) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ItemEntity search(Connection connection, String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean update(Connection connection, ItemEntity entity) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Connection connection, String id) throws SQLException {
+        return false;
+    }
+}
