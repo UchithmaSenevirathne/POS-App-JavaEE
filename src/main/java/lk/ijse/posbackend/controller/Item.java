0c990398e7fd0d46b30fa99ebdd5ebb58b87046d
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-02 23:58:22 +0530

    create init method

--- src/main/java/lk/ijse/posbackend/controller/Item.java
+++ src/main/java/lk/ijse/posbackend/controller/Item.java
@@ -1,7 +1,43 @@
 package lk.ijse.posbackend.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import lk.ijse.posbackend.bo.BOFactory;
+import lk.ijse.posbackend.bo.custom.CustomerBO;
+import lk.ijse.posbackend.bo.custom.ItemBO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @WebServlet(urlPatterns = "/item", loadOnStartup = 2)
 public class Item extends HttpServlet {
+
+    Connection connection;
+
+    static Logger logger = LoggerFactory.getLogger(Customer.class);
+
+    ItemBO itemBO = (ItemBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.ITEM);
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("Init method invoked");
+
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/POS_Backend");
+
+            this.connection = pool.getConnection();
+            logger.debug("connection initialized", this.connection);
+
+        } catch (SQLException | NamingException e) {
+            logger.error("DB connection not init");
+            e.printStackTrace();
+        }
+    }
 }
