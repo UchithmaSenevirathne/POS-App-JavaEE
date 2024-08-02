b4be096a18f7cb158a39df852b8b4e8fb2453130
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-03 00:30:22 +0530

    implement getall dao

--- src/main/java/lk/ijse/posbackend/dao/custom/impl/ItemDAOImpl.java
+++ src/main/java/lk/ijse/posbackend/dao/custom/impl/ItemDAOImpl.java
@@ -2,10 +2,13 @@ package lk.ijse.posbackend.dao.custom.impl;
 
 import lk.ijse.posbackend.dao.SQLUtil;
 import lk.ijse.posbackend.dao.custom.ItemDAO;
+import lk.ijse.posbackend.entity.CustomerEntity;
 import lk.ijse.posbackend.entity.ItemEntity;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemDAOImpl implements ItemDAO {
@@ -22,7 +25,20 @@ public class ItemDAOImpl implements ItemDAO {
 
     @Override
     public List<ItemEntity> getAll(Connection connection) throws SQLException {
-        return null;
+        List<ItemEntity> items = new ArrayList<>();
+
+        ResultSet resultSet = SQLUtil.execute(connection,"SELECT * FROM item");
+
+        while (resultSet.next()){
+            String item_id = resultSet.getString(1);
+            String item_name = resultSet.getString(2);
+            String item_desc = resultSet.getString(3);
+            Double item_up = resultSet.getDouble(4);
+
+            var entity = new ItemEntity(item_id, item_name, item_desc, item_up);
+            items.add(entity);
+        }
+        return items;
     }
 
     @Override
