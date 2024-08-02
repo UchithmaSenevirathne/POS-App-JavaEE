21c0577f5ab1534443f076c7a913bd85362b550b
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-03 00:00:03 +0530

    create dopost

--- src/main/java/lk/ijse/posbackend/controller/Item.java
+++ src/main/java/lk/ijse/posbackend/controller/Item.java
@@ -1,17 +1,24 @@
 package lk.ijse.posbackend.controller;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.posbackend.bo.BOFactory;
 import lk.ijse.posbackend.bo.custom.CustomerBO;
 import lk.ijse.posbackend.bo.custom.ItemBO;
+import lk.ijse.posbackend.dto.CutomerDTO;
+import lk.ijse.posbackend.dto.ItemDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -37,6 +44,32 @@ public class Item extends HttpServlet {
 
         } catch (SQLException | NamingException e) {
             logger.error("DB connection not init");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+        try {
+            Jsonb jsonb = JsonbBuilder.create();
+            ItemDTO itemDTO = jsonb.fromJson(req.getReader(), ItemDTO.class);
+            if(false){
+                return;
+            }
+            //save data to the db
+            boolean isSaved = itemBO.saveItem(itemDTO,connection);
+
+            if(isSaved){
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+            }else{
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "failed to save");
+            }
+
+        } catch (Exception e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             e.printStackTrace();
         }
     }
