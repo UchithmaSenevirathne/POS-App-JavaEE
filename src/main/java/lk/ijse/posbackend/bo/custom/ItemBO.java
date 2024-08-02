5a210f872dae78b134e3fbdb9b40a54740903fec
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-02 19:11:48 +0530

    implement item bo

--- src/main/java/lk/ijse/posbackend/bo/custom/ItemBO.java
+++ src/main/java/lk/ijse/posbackend/bo/custom/ItemBO.java
@@ -1,6 +1,16 @@
 package lk.ijse.posbackend.bo.custom;
 
 import lk.ijse.posbackend.bo.SuperBO;
+import lk.ijse.posbackend.dto.CutomerDTO;
+import lk.ijse.posbackend.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public interface ItemBO extends SuperBO {
+    boolean updateItem(ItemDTO dto, Connection connection) throws SQLException;
+    boolean saveItem(ItemDTO dto, Connection connection) throws SQLException;
+    List<ItemDTO> getAllItems(Connection connection) throws SQLException;
+    boolean deleteItem(String id, Connection connection) throws SQLException;
 }
--- src/main/java/lk/ijse/posbackend/bo/custom/impl/ItemBOImpl.java
+++ src/main/java/lk/ijse/posbackend/bo/custom/impl/ItemBOImpl.java
@@ -1,6 +1,30 @@
 package lk.ijse.posbackend.bo.custom.impl;
 
 import lk.ijse.posbackend.bo.custom.ItemBO;
+import lk.ijse.posbackend.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 public class ItemBOImpl implements ItemBO {
+    @Override
+    public boolean updateItem(ItemDTO dto, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean saveItem(ItemDTO dto, Connection connection) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public List<ItemDTO> getAllItems(Connection connection) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean deleteItem(String id, Connection connection) throws SQLException {
+        return false;
+    }
 }
