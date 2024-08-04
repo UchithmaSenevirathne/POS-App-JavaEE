4f680eb145920d7aaa5a7afaeeb95c12696ef2e7
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-04 06:52:42 +0530

    implement order dto and entity

--- src/main/java/lk/ijse/posbackend/dto/OrderDTO.java
+++ src/main/java/lk/ijse/posbackend/dto/OrderDTO.java
@@ -1,4 +1,14 @@
 package lk.ijse.posbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDTO {
+    private String id;
+    private String date;
+    private Double total;
 }
--- src/main/java/lk/ijse/posbackend/entity/OrderEntity.java
+++ src/main/java/lk/ijse/posbackend/entity/OrderEntity.java
@@ -1,4 +1,14 @@
 package lk.ijse.posbackend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
 public class OrderEntity {
+    private String id;
+    private String date;
+    private Double total;
 }
